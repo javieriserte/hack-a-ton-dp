4c076c3927571d7bd96223541a7f2d908a8c8097
Javier Iserte <jiserte@leloir.org.ar>
2022-09-04 13:10:00 -0300

    update test dissimilar.json

--- main.py
+++ main.py
@@ -189,7 +189,7 @@ if __name__ == '__main__':
         os.path.join(
             "data",
             "dataset",
-            "test_similar.json"
+            "test_dissimilar.json"
         ),
         orient='records',
         dtype=False
